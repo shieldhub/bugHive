eb603638b4ca2da0dc006bce16305541944218a0
shieldhub <danesh@shieldhub.in>
2018-10-15 23:46:07 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,34 +1,30 @@
 <?php
-session_start(); // Starting Session
-$error=''; // Variable To Store Error Message
-if (isset($_POST['submit'])) {
-if (empty($_POST['username']) || empty($_POST['password'])) {
-$error = "Username or Password is invalid";
-}
-else
-{
-// Define $username and $password
-$username=$_POST['username'];
-$password=$_POST['password'];
-// Establishing Connection with Server by passing server_name, user_id and password as a parameter
-$connection = mysqli_connect("localhost", "root", "mutillidae");
-// To protect MySQL injection for Security purpose
-$username = stripslashes($username);
-$password = stripslashes($password);
-$username = mysqli_real_escape_string($connection, $username);
-$password = mysqli_real_escape_string($connection, $password);
-// Selecting Database
-$db = mysqli_select_db($connection, "company");
-// SQL query to fetch information of registerd users and finds user match.
-$query = mysqli_query($connection, "select * from login where password='$password' AND username='$username'");
-$rows = mysqli_num_rows($query);
-if ($rows == 1) {
-$_SESSION['login_user']=$username; // Initializing Session
-header("location: profile.php"); // Redirecting To Other Page
-} else {
-$error = "Username or Password is invalid";
-}
-mysqli_close($connection); // Closing Connection
-}
-}
+include('session.php');
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Your Home Page</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="profile">
+<b id="welcome">Welcome : <i><?php echo $login_session; ?></i></b>
+<b id="logout"><a href="logout.php">Log Out</a></b>
+</div>
+<!-- Message box-->
+<div id="main">
+<h1>Message Box</h1>
+<div id="login">
+<h2>Message</h2>
+<form action="" method="get">
+<label>Message :</label>
+<input id="name" name="message" placeholder="message" type="text">
+<input name="submit" type="submit" value=" Send ">
+<span><?php echo $error; ?></span>
+</form>
+</div>
+</div>
+
+</body>
+</html>
