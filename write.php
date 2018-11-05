0d393e6712a96133f102778504f2a51b340e4077
shieldhub <danesh@shieldhub.in>
2018-11-05 12:13:19 +0530

    Create write.php

---
+++ write.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+if(isset($_POST['submit'])){
+$msg = $_POST['message'];
+
+$conn = mysqli_connect('localhost', 'root', 'mutillidae', 'company');
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$user = $_SESSION['login_user'];
+$sql = "INSERT INTO message (message, user)
+VALUES ('$msg', '$user')";
+
+if (mysqli_query($conn, $sql)) {
+   // echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+}else{}
+
+
+?>
