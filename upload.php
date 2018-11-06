9e1cdf81c5a65f68da7e644c6e0a1f23e55cb5f7
shieldhub <danesh@shieldhub.in>
2018-11-06 13:13:19 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -6,12 +6,21 @@
       $file_tmp =$_FILES['image']['tmp_name'];
       $file_type=$_FILES['image']['type'];
       $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
-
-      $expensions= array("jpeg","jpg","png");
+      $type=end(explode('/',$_FILES['image']['type']));
+      echo $file_type;
+      //echo $file_ext;
+    /*  $expensions= array("jpeg","jpg","png");
 
       if(in_array($file_ext,$expensions)=== false){
          $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+      }*/
+
+      $imagetype= array("png", "jpg", "jpeg");
+
+      if(in_array($type,$imagetype)=== false){
+	  $errors[]="Only images are allowed to upload.";
       }
+
 
       if($file_size > 2097152){
          $errors[]='File size must be excately 2 MB';
@@ -21,7 +30,7 @@
          move_uploaded_file($file_tmp,"images/".$file_name);
 	 echo "Success";
 
-	 print_r($_FILES);
+	// print_r($_FILES);
       }else{
          print_r($errors);
       }
